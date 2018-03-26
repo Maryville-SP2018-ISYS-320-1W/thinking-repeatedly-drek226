5a3b0060d893abfed042cb1b247e5b32bbb0fee2
drek226 <37354452+drek226@users.noreply.github.com>
2018-03-25 20:26:50 -0500

    ?

--- src/P12_NumbersLeft.java
+++ src/P12_NumbersLeft.java
@@ -2,12 +2,30 @@
   	ISYS 320
   	Name(s): Derek Rieger
   	Date: 3/25/18
+  	
+1
+22
+333
+4444
+55555
+
 */
 
 public class P12_NumbersLeft {
 
+	
 	public static void main(String[] args) {
+		 int i,j;
+	     for(i=1;i<=5;i++)
+	     {
+	         for(j=i;j>=1;j--)
+	         {
+	             System.out.print(i);
+	            }
+	            System.out.println("\n");
+	        }
+		}
+		
 		
 	}
 
-}
