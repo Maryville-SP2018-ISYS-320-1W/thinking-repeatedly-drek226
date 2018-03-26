fe8e3b8434606615bb510469014d431e840a52af
drek226 <37354452+drek226@users.noreply.github.com>
2018-03-25 22:33:15 -0500

    final draft for thinking repeatedly

--- src/P12_NumbersLeft.java
+++ src/P12_NumbersLeft.java
@@ -19,13 +19,20 @@ public class P12_NumbersLeft {
 	     for(i=1;i<=5;i++)
 	     {
 	         for(j=i;j>=1;j--)
-	         {
-	             System.out.print(i);
-	            }
-	            System.out.println("\n");
-	        }
-		}
+	     
+	        	 
+	       System.out.print(i);
+	         System.out.print("\n");
+	         }
+	     
+	             
+	         }
+	     }
+	             
+	             
+	             
+	
 		
 		
-	}
+	
 
--- src/P13_NumbersRight.java
+++ src/P13_NumbersRight.java
@@ -3,28 +3,27 @@
   	Name(s):Derek Rieger
   	Date: 3/25/18
   	
-  	?????????????????????
+  	?????????????????????   I think I need another "for" loop to space the numbers to the right but I can't figure it out
 */
 
 
 	public class P13_NumbersRight {
 
-	
 		public static void main(String[] args) {
 			 int i,j;
 		     for(i=1;i<=5;i++)
 		     {
 		         for(j=i;j>=1;j--)
-		         {
+		     
 		        	 
-		        	 System.out.print(i);
-
-			          
-		            }
-
+		       System.out.print(i);
 		         System.out.print("\n");
-		        }
-			}
+		         }
+		     
+		             
+		         }
+		     }
+		       
+		
 			
-			
-		}
+		
